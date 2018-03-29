d2602a6294743c6f6e962f8bfd2f93cd693c52e8
fanfeifan <fanff.nju@gmail.com>
2018-03-29 16:26:22 +0800

    Optimize the restart mechanism

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 This repo is used to collect the china user stats from the given repository in github.
 Now it can collect the stargazers, contributors, forks information. For more information, you can follow the [API reference](http://pygithub.readthedocs.io/en/latest/apis.html) to add more.
 Notice that there is the rate limit from Github API, in order to increase the speed, users can utilize multiple github accounts and [slices](http://pygithub.readthedocs.io/en/latest/utilities.html#github.PaginatedList.PaginatedList) to accelerate the crawling process in parallel.
+
+**The github API only support up to 40000 [pagination](https://stackoverflow.com/questions/25265465/why-github-api-gives-me-a-lower-number-stars-of-a-repo) results **
 ## requirements
-pip install PyGithub
+	$ pip install PyGithub
 ## How to use it
-python crawler.py
+	$ python crawler.py
--- crawler.py
+++ crawler.py
@@ -4,8 +4,9 @@ import json
 import os
 
 class Crawler():
-  def __init__(self, login_id, login_passwd, user, repo):
-    self.gh = Github(login_id, login_passwd)
+  def __init__(self, login_id, login_passwd, user, repo, per_page=10):
+    self.per_page = per_page
+    self.gh = Github(login_id, login_passwd, per_page=self.per_page)
     self.city_country = self.getCityData()
     self.repo = self.gh.get_user(user).get_repo(repo)
  
@@ -31,40 +32,41 @@ class Crawler():
       line = fr_restart.readline()
       if line:
         restart, china_count, total_valid_count = [int(i) for i in line.strip().split(',')]
-    count = restart
+    count = restart * self.per_page
+    cur_page = restart
 
-    for user in collection[restart:]:
-      if isForks:
-        user = user.owner
-      count += 1
-      print 'processed: %d/%d, china user in valid users: %d/%d' % (count, total_count, china_count, total_valid_count)
-      # store the progress
-      if count % 10 == 0:
-        fw_restart = open(restart_file, 'w')
-        fw_restart.write('%d,%d,%d' %(count, china_count, total_valid_count))
-        fw_restart.close()
-      location = user.location
-      if location is None:continue
-      location = location.lower()
-      total_valid_count += 1
-      check = False
-      if location.find('china') >= 0:
-        china_count += 1
-        check = True
-      else:
-        token = location.split(',')
-        if self.city_country.has_key(token[0]) and self.city_country[token[0]]=='CN':
+    while True:
+      for user in collection.get_page(cur_page):
+        if isForks:
+          user = user.owner
+        count += 1
+        print 'processed: %d/%d, china user in valid users: %d/%d' % (count, total_count, china_count, total_valid_count)
+        # store the progress
+        if count % 10 == 0:
+          fw_restart = open(restart_file, 'w')
+          fw_restart.write('%d,%d,%d' %(int(count/self.per_page), china_count, total_valid_count))
+          fw_restart.close()
+        location = user.location
+        if location is None:continue
+        location = location.lower()
+        total_valid_count += 1
+        check = False
+        if location.find('china') >= 0:
           china_count += 1
           check = True
-      if check and storeUserInfo:
-        user_info = '%s##%s##%s##%s##%s\n' % (user.login, user.name, user.email, user.followers, location)
-        print user_info
-        fw = open(log_file, 'a')
-        fw.write(user_info.encode('utf8'))
-        fw.close()
-
-    print '%d,%d,%d' % (china_count, total_valid_count, total_count)
-
+        else:
+          token = location.split(',')
+          if self.city_country.has_key(token[0]) and self.city_country[token[0]]=='CN':
+            china_count += 1
+            check = True
+        if check and storeUserInfo:
+          user_info = '%s##%s##%s##%s##%s\n' % (user.login, user.name, user.email, user.followers, location)
+          print user_info
+          fw = open(log_file, 'a')
+          fw.write(user_info.encode('utf8'))
+          fw.close()
+      # iterate next page
+      cur_page += 1
 
   def crawlStargazer(self, storeUserInfo):
     stargazers = self.repo.get_stargazers()

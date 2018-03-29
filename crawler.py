610a22cdb67847cb4bb19363f72f95ddfe39461d
fanfeifan <fanff.nju@gmail.com>
2018-03-29 17:50:23 +0800

    add retry and switch account

--- crawler.py
+++ crawler.py
@@ -2,13 +2,27 @@
 from github import Github
 import json
 import os
+import random
 
 class Crawler():
-  def __init__(self, login_id, login_passwd, user, repo, per_page=10):
+  def __init__(self, user, repo, per_page=30, max_retry_times=5):
     self.per_page = per_page
-    self.gh = Github(login_id, login_passwd, per_page=self.per_page)
     self.city_country = self.getCityData()
-    self.repo = self.gh.get_user(user).get_repo(repo)
+    self.repo_name = repo
+    self.user_name = user
+    self.max_retry_times = max_retry_times
+    self.account = []
+    with open('./accounts.txt', 'r') as fr:
+      for line in fr:
+        self.account.append(line.strip())
+    self.chooseOneAccount()
+
+  def chooseOneAccount(self):
+    idx = random.randint(0, len(self.account)-1)
+    login_id, login_passwd = self.account[idx].split(',')
+    print 'choose one github account:', login_id, login_passwd
+    self.gh = Github(login_id, login_passwd, per_page=self.per_page)
+    self.repo = self.gh.get_user(self.user_name).get_repo(self.repo_name)
  
   def getCityData(self):
     f = open('./cities.json','r')
@@ -20,7 +34,7 @@ class Crawler():
     return city_country
 
 
-  def parseCollection(self, collection, collection_size, storeUserInfo, log_file, isForks, restart_file):
+  def parseCollection(self, collection, collection_size, storeUserInfo, log_file, flag, restart_file):
     china_count = 0
     total_valid_count = 0
     total_count = collection_size
@@ -37,8 +51,11 @@ class Crawler():
 
     while True:
       for user in collection.get_page(cur_page):
-        if isForks:
+        if flag == 'fork':
           user = user.owner
+        elif flag == 'stargazer':
+          # print 'starred_time: ', user.starred_at, user.user.name
+          user = user.user
         count += 1
         print 'processed: %d/%d, china user in valid users: %d/%d' % (count, total_count, china_count, total_valid_count)
         # store the progress
@@ -69,22 +86,41 @@ class Crawler():
       cur_page += 1
 
   def crawlStargazer(self, storeUserInfo):
-    stargazers = self.repo.get_stargazers()
-    size = self.repo.stargazers_count
-    self.parseCollection(stargazers, size, storeUserInfo, './stargazer.user', False, './stargazer.restart')
+    retry_times = 0
+    while retry_times < self.max_retry_times:
+      stargazers = self.repo.get_stargazers_with_dates()
+      size = self.repo.stargazers_count
+      try:
+        self.parseCollection(stargazers, size, storeUserInfo, './stargazer.user', 'stargazer', './stargazer.restart')
+      except Exception, e:
+        self.chooseOneAccount()
+        print 'retry %d and changed the account...' % (retry_times)
 
   def crawlContributor(self, storeUserInfo):
-    contributors = self.repo.get_contributors()
-    self.parseCollection(contributors, -1, storeUserInfo, './contributor.user', False, './contributor.restart')
+    retry_times = 0
+    while retry_times < self.max_retry_times:
+      try:
+        contributors = self.repo.get_contributors()
+        self.parseCollection(contributors, -1, storeUserInfo, './contributor.user', 'contributor', './contributor.restart')
+      except Exception, e:
+        self.chooseOneAccount()
+        print 'retry %d and changed the account...' % (retry_times)
 
   def crawlForks(self, storeUserInfo):
-    forks = self.repo.get_forks()
-    size = self.repo.forks_count
-    self.parseCollection(forks, size, storeUserInfo, './forks.user', True, './forks.restart')
+    retry_times = 0
+    while retry_times < self.max_retry_times:
+      try:
+        forks = self.repo.get_forks()
+        size = self.repo.forks_count
+        self.parseCollection(forks, size, storeUserInfo, './forks.user', 'fork', './forks.restart')
+      except Exception, e:
+        self.chooseOneAccount()
+        print 'retry %d and changed the account...' % (retry_times)
+
 
 
 if __name__ == '__main__':
-  crawler = Crawler('testaccount135', 'testaccount123', 'tensorflow', 'tensorflow')
+  crawler = Crawler('tensorflow', 'tensorflow')
   crawler.crawlStargazer(True)
   #crawler.crawlContributor(False)
   #crawler.crawlForks(True)
